22cd3a640e49dd9a00f5a2ac3bfb1695922aa13e
Koki Rizov <kalo282828@gmail.com>
2024-08-30 17:37:47 +0300

    made the functions for populating the screen

--- script.js
+++ script.js
@@ -35,3 +35,66 @@ function operate(a , b, operand){
         return divide(a,b)
     }
 }
+
+function clickOne() {
+    const screen = document.querySelector(".screen");
+    const num = document.querySelector("#one");
+    num.addEventListener("click", () => { screen.innerHTML += "1"; });
+}
+
+function clickTwo() {
+    const screen = document.querySelector(".screen");
+    const num = document.querySelector("#two");
+    num.addEventListener("click", () => { screen.innerHTML += "2"; });
+}
+
+function clickThree() {
+    const screen = document.querySelector(".screen");
+    const num = document.querySelector("#three");
+    num.addEventListener("click", () => { screen.innerHTML += "3"; });
+}
+
+function clickFour() {
+    const screen = document.querySelector(".screen");
+    const num = document.querySelector("#four");
+    num.addEventListener("click", () => { screen.innerHTML += "4"; });
+}
+
+function clickFive() {
+    const screen = document.querySelector(".screen");
+    const num = document.querySelector("#five");
+    num.addEventListener("click", () => { screen.innerHTML += "5"; });
+}
+
+function clickSix() {
+    const screen = document.querySelector(".screen");
+    const num = document.querySelector("#six");
+    num.addEventListener("click", () => { screen.innerHTML += "6"; });
+}
+
+function clickSeven() {
+    const screen = document.querySelector(".screen");
+    const num = document.querySelector("#seven");
+    num.addEventListener("click", () => { screen.innerHTML += "7"; });
+}
+
+function clickEight() {
+    const screen = document.querySelector(".screen");
+    const num = document.querySelector("#eight");
+    num.addEventListener("click", () => { screen.innerHTML += "8"; });
+}
+
+function clickNine() {
+    const screen = document.querySelector(".screen");
+    const num = document.querySelector("#nine");
+    num.addEventListener("click", () => { screen.innerHTML += "9"; });
+}
+clickOne();
+clickTwo();
+clickThree();
+clickFour();
+clickFive();
+clickSix();
+clickSeven();
+clickEight();
+clickNine();
