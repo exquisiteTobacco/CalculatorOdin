93b2191dd880e7e7816fc68e4fbcff897dceec82
Koki Rizov <kalo282828@gmail.com>
2024-08-30 13:47:35 +0300

    made the operate function and the input variables.

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ function multiply(a, b){
 function divide(a, b){
  return a / b;
 }
+// basic input for the calc
+let numA = 0;
+let numB = 0;
+let operator = "";
+
+// function for calling the math functions on certain numbers
+function operate(a , b, operand){
+    if(operand === "+")
+    {
+        return add(a,b)
+    }
+    else if (operand === "-")
+    {
+        return subtract(a,b)
+    }
+    else if (operand === "*")
+    {
+        return multiply(a,b)
+    }
+    else
+    {
+        return divide(a,b)
+    }
+}
